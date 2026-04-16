a2163da4ed0e5a37f9f9d70d0a688df3b49fb62f
boccatosantos85 <229978166+boccato85@users.noreply.github.com>
2026-04-16 17:11:17 -0300

    Revert "test: trigger ci failure"
    This reverts commit dd525860af9d1f7cdec21eb796d539bb524d29db.

--- agent/main_test.go
+++ agent/main_test.go
@@ -45,8 +45,3 @@ func TestGetPodRequest_EmptyMap(t *testing.T) {
 		t.Error("expected found=false for nil map")
 	}
 }
-
-func TestFailingCI(t *testing.T) {
-	t.Errorf("CI pipeline failure test")
-}
-
